715f87e9bfe649bdb9c518c636ea5e438b50a2f4
Samuli Löf <samuli.lof@gmail.com>
2016-09-25 13:14:10 +0300

    Window title fix

--- Fangame Manager/AutohotkeyScriptManager.cs
+++ Fangame Manager/AutohotkeyScriptManager.cs
@@ -43,7 +43,7 @@ namespace Fangame_Manager
             }
             catch (Exception e)
             {
-                MessageBox.Show("Error happened while starting: " + filePath + " with error: " + e.Message);
+                MessageBox.Show("Error happened while starting: " + filePath + " with error: " + e.Message, "Error!");
             }
         }
     }
